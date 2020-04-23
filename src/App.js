46b0341c930277a448748431a71c6a58f10cef80
bbest31 <brandonbest94@live.ca>
2020-04-23 11:43:46 -0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import HeaderSection from './HeaderSection/HeaderSection';
 import AboutSection from './AboutSection/AboutSection';
 import PortfolioSection from './PortfolioSection/PortfolioSection';
 import FooterSection from './FooterSection/FooterSection';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App main-bg">
-        <HeaderSection />
-        <AboutSection />
-        <PortfolioSection />
-        <FooterSection />
+      <NavBar />
+      <HeaderSection />
+      <AboutSection />
+      <PortfolioSection />
+      <FooterSection />
     </div>
   );
 }
